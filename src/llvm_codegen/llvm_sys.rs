e72b1bfe35f025c11007f191431a77605b479638
Javad Asgari Shafique <me@javad.sh>
2025-03-12 15:04:00 +0100

    Fix llvm_sys 201 pkg name

--- src/llvm_codegen/llvm_sys.rs
+++ src/llvm_codegen/llvm_sys.rs
@@ -1,6 +1,6 @@
 #[cfg(feature = "llvm19")]
 pub use llvm_sys_191 as llvm_sys;
 #[cfg(feature = "llvm20")]
-pub use llvm_sys_20 as llvm_sys;
+pub use llvm_sys_201 as llvm_sys;
 
 pub use llvm_sys::*;
